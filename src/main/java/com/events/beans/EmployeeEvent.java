2c681431d3d0226c2129c1d39a45f0a986950e9a
Sreenu07 <48574126+Sreenu07@users.noreply.github.com>
2019-03-20 12:02:02 +0530

    optimized code

--- src/main/java/com/events/beans/EmployeeEvent.java
+++ src/main/java/com/events/beans/EmployeeEvent.java
@@ -1,19 +1,9 @@
 package com.events.beans;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
-@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id", scope = EmployeeEvent.class)
 public class EmployeeEvent {
-	private String id;
 	private String uuid;
 	private String eventName;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
+	
 	public String getUuid() {
 		return uuid;
 	}
