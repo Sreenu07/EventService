9ccf0613b5edd212fa19abd7591765206dda962a
Sreenu07 <48574126+Sreenu07@users.noreply.github.com>
2019-03-20 12:00:48 +0530

    added Json bean

--- src/main/java/com/events/EventServiceApplication.java
+++ src/main/java/com/events/EventServiceApplication.java
@@ -1,11 +1,17 @@
 package com.events;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventServiceApplication.class, args);
 	}
+	 @Bean
+	 public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+	        return new Jackson2JsonMessageConverter();
+	    }
 }
